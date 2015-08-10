5d4c00698bfb96ecc5d722b80501269a87242244
ronald_edelschaap <ronald_edelschaap@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-10 11:04:21 +0000

    Version 1.0: First release
    git-svn-id: https://plugins.svn.wordpress.org/web-whales-seoshop-sign-up-form/tags/1.0@1217077 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ assets/ww-seoshop-sign-up.js
@@ -0,0 +1,71 @@
+(function( $ ) {
+	$( document ).on( 'submit', 'form.ww_seoshop-sign-up-form', function( event ) {
+		event.preventDefault();
+
+		var form = $( this ), btn = $( '[type=submit]', form ), msg_box = $( '.ww_message-box', form );
+
+		$.ajax( {
+			beforeSend : function() {
+				$( '[name]', form ).removeClass( 'form-error' );
+				btn.prop( 'disabled', true );
+				form.addClass( 'busy' );
+			}, cache   : false, complete : function( data, status ) {
+				var r = data.responseJSON;
+				r.callback_success = r.callback_success || false;
+				r.error_fields = r.error_fields || false;
+				r.message = r.message || '';
+				r.status = r.status || false;
+
+				if ( r.status ) {
+					$( '.ww_form-rows', form ).slideUp( function() {
+						$( this ).remove()
+					} );
+				} else if ( typeof r.error_fields == 'object' ) {
+					$.each( r.error_fields, function( i, v ) {
+						$( '[name=' + v + ']', form ).addClass( 'form-error' );
+					} );
+
+					if ( typeof window['grecaptcha'] != 'undefined' ) {
+						grecaptcha.reset();
+					}
+				}
+
+				if ( r.message != '' ) {
+					msg_box.html( r.message ).slideDown( function() {
+						$( this ).trigger( 'change.seoshop-sign-up-form' );
+
+						btn.prop( 'disabled', false );
+						form.removeClass( 'busy' );
+
+						if ( r.status && r.callback_success !== false ) {
+							eval( r.callback_success );
+						}
+					} );
+				} else {
+					msg_box.slideUp( function() {
+						$( this ).html( '' );
+
+						btn.prop( 'disabled', false );
+						form.removeClass( 'busy' );
+
+						if ( r.status && r.callback_success !== false ) {
+							eval( r.callback_success );
+						}
+					} );
+				}
+			}, data    : form.serializeArray(), dataType : 'json', type : 'POST'
+		} );
+	} ).on( 'change.seoshop-sign-up-form', '.ww_message-box', function() {
+		var _this = $( this ), offset = 50 + ( $( '#wpadminbar' ).outerHeight( true ) || 0 ), pos = _this.offset().top, w_top = $( window ).scrollTop();
+
+		if ( typeof window['ww_seoshop_options'] != 'undefined' ) {
+			offset += parseInt( ww_seoshop_options.sticky_header_offset || 0 );
+		}
+
+		pos -= offset;
+
+		if ( pos < w_top ) {
+			$( 'html, body' ).animate( {scrollTop : pos}, Math.abs( w_top - pos ) * 3.5 );
+		}
+	} );
+})( jQuery );
---
+++ readme.txt
@@ -0,0 +1,32 @@
+=== SEOshop Sign Up Form by Web Whales ===
+Contributors: ronald_edelschaap
+Tags: seoshop,affiliate,ecommerce,e-commerce
+Requires at least: 4.1
+Tested up to: 4.3
+Stable tag: 1.0
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Provides an affiliate sign up form for SEOshop Partners.
+
+== Description ==
+This plugin provides an affiliate sign up form for SEOshop Partners. With this sign up form your clients and visitors can request a 14-days trial store at SEOshop. The stores registered with this form automatically show up in your SEOshop Partner Dashboard. To avoid spam sign ups, this plugin has a built in reCAPTCHA by Google functionality.
+
+Not yet a SEOshop Partner? Request a partnership for free at [SEOshop](http://www.getseoshop.com/partners/?utm_source=Web%20Whales&utm_medium=referral&utm_campaign=Web%20Whales%20SEOshop%20Sign%20Up%20WordPress%20plugin).
+
+The plugin language is English. Dutch language files are included. Feel free to send your translation to me, so I can include it in the plugin.
+
+Proudly presented to you by [Web Whales](https://webwhales.nl/).
+
+== Installation ==
+1. Download the archive and unzip it in /wp-content/plugins, upload the archive at Plugins > Add New > Upload Plugin or install via Plugins > Add New > Search for the plugin name in the search bar.
+2. Activate the plugin through the Plugins menu in WordPress
+3. Update the plugin settings in with the settings page located in the WordPress settings menu.
+
+== Frequently Asked Questions ==
+= The sign up form doesn't fit well in my theme. Is there anyway to adjust the form? =
+Yes there is! Just copy the templates/shortcodes/sign_up_form.phtml to /wp-content/themes/<your (child) theme>/seoshop-sign-up/shortcodes/ and modify it. Please make sure you don't remove any classes or input names to keep the form working.
+
+== Changelog ==
+= 1.0 =
+First release
